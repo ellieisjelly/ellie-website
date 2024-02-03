41c5ca9940a0ca39640e806f6323be1f5078e7a0
ellieisjelly <ellie@ellieis.me>
2024-02-03 15:47:41 -0300

    doge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,36 @@
+import { createEffect } from "solid-js";
 import Topbar, { TopbarButtons } from "../components/Topbar";
 
 export default function () {
+    createEffect(() => {
+        let isPlaying = false;
+        setInterval(() => {
+            if (!isPlaying) {
+                console.log("chance");
+                if (Math.random() > 0.9) {
+                    console.log("playing");
+                    isPlaying = true;
+                    const doge = document.getElementById("doge");
+                    if (doge) {
+                        if (window.innerWidth < 1280) {
+                            doge.className = "doge playFast";
+                        } else {
+                            doge.className = "doge play";
+                        }
+                        setTimeout(() => {
+                            isPlaying = false;
+                            doge.className = "doge";
+                        }, 10000);
+                    }
+                }
+            }
+        }, 100);
+    });
     return (
-        <div>
+        <div class="fade">
             <Topbar activeButton={TopbarButtons.Home} />
             <div class="text-center py-12">
-                <h1 class="text-8xl text-lavender" id="name">
+                <h1 class="text-8xl" id="name">
                     Hi, I'm Ellie!
                 </h1>
                 <p class="py-2 text-xl">
@@ -13,8 +38,17 @@ export default function () {
                     <br />
                     <span class="text-overlay0">...</span>
                     <br />
-                    <span class="text-overlay0 text-sm">Not a lot here</span>
+                    <span class="text-overlay0 text-sm">Not a lot here..</span>
                 </p>
+                <p class="py-8">Check back later for more!</p>
+                <img
+                    src="/doge.webp"
+                    height={256}
+                    width={256}
+                    alt="doge"
+                    id="doge"
+                    class="doge"
+                />
             </div>
         </div>
     );
