8cf061a6c0cda7a961b61e0cf214bd89551f1cfc
ellieisjelly <ellie@ellieis.me>
2024-02-03 13:24:20 -0300

    begin home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,20 @@
 import Topbar, { TopbarButtons } from "../components/Topbar";
+
 export default function () {
     return (
         <div>
             <Topbar activeButton={TopbarButtons.Home} />
             <div class="text-center py-12">
-                <h1 class="text-4xl text-lavender">Hi, I'm Ellie!</h1>
+                <h1 class="text-8xl text-lavender" id="name">
+                    Hi, I'm Ellie!
+                </h1>
+                <p class="py-2 text-xl">
+                    You can find a lot of cool stuff here!
+                    <br />
+                    <span class="text-overlay0">...</span>
+                    <br />
+                    <span class="text-overlay0 text-sm">Not a lot here</span>
+                </p>
             </div>
         </div>
     );
