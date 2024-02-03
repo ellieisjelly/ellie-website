cddb1a027b537d2450344b4f68ad70bc08f1ca5c
ellieisjelly <ellie@ellieis.me>
2024-02-03 12:55:29 -0300

    Roboto font and topbar looks good now

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -6,30 +6,33 @@ enum TopbarButtons {
 }
 
 function TopbarButton(props: { name: string; href: string; active: boolean }) {
-    const [active, setActive] = createSignal(props.active);
+    const [active, setActive] = createSignal(props.active); // eslint-disable-line solid/reactivity
     return (
         <A
             href={props.href}
-            class={`${
-                active() ? "bg-crust" : "bg-mantle"
-            } hover:bg-crust px-2 py-2`}
+            class={`hover:text-sky px-3 py-3
+            ${active() ? "text-sky" : "text-blue"}`}
             onClick={() => setActive(true)}
         >
             {props.name}
         </A>
     );
 }
+function Separator() {
+    return <div class="bg-surface0 min-w-[2px] min-h-8 mx-[8px]" />;
+}
 function isActive(button: TopbarButtons, activeButton: TopbarButtons) {
     return button === activeButton;
 }
 export default function (props: { activeButton: TopbarButtons }) {
     return (
-        <div class="bg-mantle backdrop-blur-sm border-b-crust w-full h-12 flex flex-row-reverse gap-4 sticky top-0">
+        <div class="bg-mantle backdrop-blur-sm border-b-crust w-full h-12 flex flex-row-reverse gap-2 items-center sticky top-0">
             <TopbarButton
                 name="Home"
                 href="/home"
                 active={isActive(TopbarButtons.Home, props.activeButton)}
             />
+            <Separator />
             <TopbarButton
                 name="About"
                 href="/about"
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,9 @@ export default function () {
     return (
         <div>
             <Topbar activeButton={TopbarButtons.Home} />
+            <div class="text-center py-12">
+                <h1 class="text-4xl text-lavender">Hi, I'm Ellie!</h1>
+            </div>
         </div>
     );
 }
