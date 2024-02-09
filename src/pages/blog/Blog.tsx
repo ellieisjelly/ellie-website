eea1de7c9b9378a9456ea17c9da8211c73f1bca0
ellieisjelly <ellie@ellieis.me>
2024-02-09 11:17:34 -0300

    Posts are almost rendering correctly now

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,6 +1,6 @@
 import Topbar, { TopbarButtons } from "../../components/Topbar";
 import BlogPost, { Post } from "../../components/BlogPost";
-import { createResource, For } from "solid-js";
+import { createResource, For, Suspense } from "solid-js";
 import { apiUrl } from "../../App";
 
 type getPosts = {
@@ -22,13 +22,19 @@ export default function () {
     return (
         <div class="fade text-left">
             <Topbar activeButton={TopbarButtons.Blog} />
-            <div class="max-w-2/3 lg:max-w-1/2 right-0 left-0 m-auto">
-                <div class="flex flex-wrap flex-col lg:flex-row py-12 gap-4 px-12 justify-center">
-                    <For each={posts()}>
-                        {(post: Post) => <BlogPost post={post} />}
-                    </For>
+            <Suspense
+                fallback={
+                    <h1 class="py-12 px-4 text-center text-2xl">Loading...</h1>
+                }
+            >
+                <div class="max-w-2/3 lg:max-w-1/2 right-0 left-0 m-auto">
+                    <div class="flex flex-wrap flex-col lg:flex-row py-12 gap-4 px-12 justify-center">
+                        <For each={posts()}>
+                            {(post: Post) => <BlogPost post={post} />}
+                        </For>
+                    </div>
                 </div>
-            </div>
+            </Suspense>
         </div>
     );
 }
--- src/pages/blog/PostViewer.tsx
+++ src/pages/blog/PostViewer.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "@solidjs/router";
 import Topbar, { TopbarButtons } from "../../components/Topbar";
-import { Show, Suspense, createResource } from "solid-js";
+import { Suspense, createResource } from "solid-js";
 import { apiUrl } from "../../App";
 import type { Post } from "../../components/BlogPost";
 import { marked } from "marked";
@@ -11,12 +11,17 @@ function renderPost(post?: Post) {
     if (!post) {
         return;
     }
+    const [parsedContent] = createResource(async () => {
+        return await marked.parse(post.content);
+    });
     return (
         <div class="px-4 py-12 md:px-96">
             <h1 class="text-2xl text-center">{post.title}</h1>
             <img src={post.image} class="rounded-3xl w-[90%] m-auto py-2" />
 
             <hr class="py-4" />
+            {/*eslint-disable-next-line solid/no-innerhtml*/}
+            <section innerHTML={parsedContent()} />
         </div>
     );
 }
@@ -27,11 +32,13 @@ export default function () {
             await fetch(apiUrl + "getPost", {
                 headers: { "Content-Type": "application/json" },
                 method: "POST",
-                body: JSON.stringify({ _id: params.id }),
+                body: JSON.stringify({ id: params.id }),
             })
         ).json();
+        console.log(response);
         return response.post;
     });
+    console.log(post());
     return (
         <div>
             <Topbar activeButton={TopbarButtons.Blog} />
@@ -40,6 +47,7 @@ export default function () {
                     <h1 class="py-12 px-4 text-center text-2xl">Loading...</h1>
                 }
             >
+                {/* To-Do: figure out how to shadow root */}
                 {renderPost(post())}
             </Suspense>
         </div>
